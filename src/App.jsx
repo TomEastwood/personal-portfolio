852b9b29559600554ef619f06e019ee10a286cd6
TomEastwood <t.eastwood99@gmail.com>
2023-11-24 14:14:44 +0000

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className = "container">
-      <Parallax pages={4}>
+      <Parallax pages={5}>
         
         <ParallaxLayer className='header' offset={0} speed={0} factor={3}>
           <div className='bg-1'></div>
@@ -33,8 +33,10 @@ function App() {
         <ParallaxLayer className='skills' offset={1.99} speed={0.7} factor={0}>
           <Skills />
         </ParallaxLayer>
-
-        <ParallaxLayer className="projects-bg" offset={2.5} speed={0.1} factor={1}>
+        <ParallaxLayer className="waves-2" offset={2.3} speed={0.1} factor={1}>
+          <img src={waves} alt="waves2" />
+        </ParallaxLayer>
+        <ParallaxLayer className='projects' offset={2.48} speed={0.1} factor={4}>
           <Projects />
         </ParallaxLayer>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,49 @@
+import brewery from '../assets/brewery.jpg';
+import dashboard from '../assets/dashboard.png';
+import '../styles/projects.css';
+
 export default function Projects () {
     return (
-        <section className = "projects">
-            <div className = "title">
-                <h2>Projects</h2>
+        <section className = "project-container">
+            <div className = "project-section-title">
+                <h1>Projects</h1>
+            </div>
+            <div className = "descriptions">
+                <p> Here you will find some of my recent projects that I have been working on, feel free to click any of the titles or images provided 
+                    to learn more about a specific project.
+                </p>
+            </div>
+            <div className = "projects-list">
+                <div className = "project">
+                    <div className = "project-image">
+                        <img src = {brewery} alt = "brewery" />
+                    </div>
+                    <div className = "project-title">
+                        <h3>JSDOM Brewery API</h3>
+                    </div>
+                    <div className = "project-description">
+                        <p>
+                            Using API requests, I created a website that displays information about breweries in the United States.
+                            The user can search for a brewery by name, city, or state. The user can also filter the results by brewery type.
+                        </p>
+                    </div>
+                </div>
+                <div className = "project">
+                    <div className = "project-image">
+                        <img src = {dashboard} alt = "dashboard" />
+                    </div>
+                    <div className = "project-title">
+                        <h3>React Dashboard</h3>
+                    </div>
+                    <div className = "project-description">
+                        <p>
+                            Using React and React Router, I created a dashboard that displays information about a user's account.
+                            The user can navigate to different pages using the navigation bar at the side of the page, view different
+                            posts and their respective comments, and view an individual post and its comments.
+
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
     )
